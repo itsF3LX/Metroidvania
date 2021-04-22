0a02c59e0fe1a2df6e5ba5e93e72b96b588a4246
SalarsaZee <74618430+SalarsaZee@users.noreply.github.com>
2021-04-23 00:45:44 +0200

    CamaraFollow
    Added CamaraFollow script
    -> make main cam follow Player

---
+++ Metroidvania/Assets/Scripts/CamaraFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    void FixedUpdate(){
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
